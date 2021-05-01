9a82bb1207b76071c939260773358707adee74f3
SenaitEkubai <Senaitekubai@gmail.com>
2021-05-01 17:08:17 +0100

    allowed acces control allow origin

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+}); /* 
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
   })
-);
+); */
 
 const Harrow = require("./challenge-london-mini-guide/data/Harrow.json");
 const Heathrow = require("./challenge-london-mini-guide/data/Heathrow.json");
